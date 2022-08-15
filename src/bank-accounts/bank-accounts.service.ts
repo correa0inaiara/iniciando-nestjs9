12727e58716c5242748c26dafbf62b346aab0c94
Naiara Correa <correa0inaiara@gmail.com>
2022-08-14 21:55:22 -0300

    Projeto Bank Accounts com Nest.js 9

---
+++ src/bank-accounts/bank-accounts.service.ts
@@ -0,0 +1,63 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { getDataSourceToken, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { CreateBankAccountDto } from './dto/create-bank-account.dto';
+import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
+import { BankAccount } from './entities/bank-account.entity';
+
+@Injectable()
+export class BankAccountsService {
+
+  constructor(
+    @InjectRepository(BankAccount)
+    private repo: Repository<BankAccount>,
+    @Inject(getDataSourceToken())
+    private dataSource: DataSource
+  ) {}
+
+  async create(createBankAccountDto: CreateBankAccountDto) {
+    const bankAccount = this.repo.create({
+      account_number: createBankAccountDto.account_number,
+      balance: 0,
+    });
+
+    await this.repo.insert(bankAccount);
+    return bankAccount;
+  }
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  findOne(id: string) {
+    return this.repo.findOneBy({id});
+  }
+
+  update(id: string, updateBankAccountDto: UpdateBankAccountDto) {
+    return `This action updates a #${id} bankAccount`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} bankAccount`;
+  }
+
+  async transfer(from: string, to: string, amount: number) {
+    // modo transação
+    const queryRunner = this.dataSource.createQueryRunner();
+    try {
+      await queryRunner.startTransaction();
+      const fromAccount = await this.repo.findOneBy({id: from})
+      const toAccount = await this.repo.findOneBy({id: to})
+
+      fromAccount.balance -= amount;
+      toAccount.balance += amount;
+
+      this.repo.save(fromAccount);
+      this.repo.save(toAccount);
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+    }
+    
+  }
+}
